de30adf6facd547dea5937f7e16350a9271fafaa
s55549 <s55549@edu.campus02.at>
2025-01-17 20:55:05 +0100

    commit

---
+++ src/test/java/org/campus02/walzl/RecapTest.java
@@ -0,0 +1,22 @@
+package org.campus02.walzl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RecapTest {
+    // 1. Setup
+    // 2. Test schreiben (execute)
+    // 3. Verifizieren
+    // 4. tear down (alles zurücksetzen)
+
+    @BeforeEach
+    void setup(){
+    }
+
+    @Test
+    void firstTest(){
+        Assertions.assertTrue(true);
+    }
+}
+
